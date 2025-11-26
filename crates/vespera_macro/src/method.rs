37d95775cdf00bb60415c0838b64a8b1e8cf8db2
owjs3901 <owjs3901@gmail.com>
2025-11-26 16:05:50 +0900

    Add vespera to http method

--- crates/vespera_macro/src/method.rs
+++ crates/vespera_macro/src/method.rs
@@ -5,14 +5,13 @@ use vespera_core::route::HttpMethod;
 /// Convert HttpMethod to axum routing TokenStream
 pub fn http_method_to_token_stream(method: HttpMethod) -> TokenStream {
     match method {
-        HttpMethod::Get => quote! { axum::routing::get },
-        HttpMethod::Post => quote! { axum::routing::post },
-        HttpMethod::Put => quote! { axum::routing::put },
-        HttpMethod::Patch => quote! { axum::routing::patch },
-        HttpMethod::Delete => quote! { axum::routing::delete },
-        HttpMethod::Head => quote! { axum::routing::head },
-        HttpMethod::Options => quote! { axum::routing::options },
-        HttpMethod::Trace => quote! { axum::routing::trace },
+        HttpMethod::Get => quote! { vespera::axum::routing::get },
+        HttpMethod::Post => quote! { vespera::axum::routing::post },
+        HttpMethod::Put => quote! { vespera::axum::routing::put },
+        HttpMethod::Patch => quote! { vespera::axum::routing::patch },
+        HttpMethod::Delete => quote! { vespera::axum::routing::delete },
+        HttpMethod::Head => quote! { vespera::axum::routing::head },
+        HttpMethod::Options => quote! { vespera::axum::routing::options },
+        HttpMethod::Trace => quote! { vespera::axum::routing::trace },
     }
 }
-
