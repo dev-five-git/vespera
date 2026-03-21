4fc0a242b01646460f53676e2b03484f721b72ab
owjs3901 <owjs3901@gmail.com>
2026-03-21 19:49:42 +0900

    Exclude coverage

--- crates/vespera_jni/src/lib.rs
+++ crates/vespera_jni/src/lib.rs
@@ -24,14 +24,16 @@ pub use vespera_inprocess;
 #[macro_export]
 macro_rules! jni_app {
     ($factory:expr) => {
-        #[cfg(not(tarpaulin_include))]
         #[unsafe(no_mangle)]
         pub extern "system" fn JNI_OnLoad(
             _vm: $crate::jni::JavaVM,
             _: *mut ::std::ffi::c_void,
         ) -> $crate::jni::sys::jint {
-            $crate::vespera_inprocess::register_app($factory);
-            $crate::jni::sys::JNI_VERSION_1_8
+            #[cfg(not(tarpaulin_include))]
+            {
+                $crate::vespera_inprocess::register_app($factory);
+                $crate::jni::sys::JNI_VERSION_1_8
+            }
         }
     };
 }
