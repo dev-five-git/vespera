c3491e36b8706f1726ba7aa8509922fbd8203eb1
Hanbin Cho <forestream@devfive.kr>
2026-04-24 10:22:33 +0900

    Feat header selected tab ui

--- apps/front/src/components/header/menu.tsx
+++ apps/front/src/components/header/menu.tsx
@@ -1,14 +1,24 @@
+'use client'
 import { Center, Text } from '@devup-ui/react'
+import { usePathname } from 'next/navigation'
 
-export function Menu({ children }: { children?: React.ReactNode }) {
+export function Menu({
+  value,
+  children,
+}: {
+  value: string
+  children?: React.ReactNode
+}) {
+  const pathname = usePathname()
+  const isSelected = pathname.includes(value)
+
   return (
     <Center cursor="pointer" px="$spacingSpacing24" py="$spacingSpacing08">
       <Text
-        // selected: '$vesperaPrimary',
         _hover={{
           color: '$textSub',
         }}
-        color="$menutext"
+        color={isSelected ? '$vesperaPrimary' : '$menutext'}
         transition="all .1s"
         typography="menu"
         whiteSpace="nowrap"
