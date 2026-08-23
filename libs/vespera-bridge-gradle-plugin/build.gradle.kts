663e9b6f0bfa7674b38d01c17b0e2845e70bf13c
devfive <devfive@devfive.kr>
2026-08-23 19:27:45 +0900

    Prepare coordinated 0.3.1 release

--- libs/vespera-bridge-gradle-plugin/build.gradle.kts
+++ libs/vespera-bridge-gradle-plugin/build.gradle.kts
@@ -27,7 +27,7 @@ tasks.named("publishPlugins") {
 }
 
 group = "kr.devfive"
-version = "0.3.1"
+version = "0.3.0"
 
 java {
     toolchain {
--- libs/vespera-bridge/build.gradle.kts
+++ libs/vespera-bridge/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "kr.devfive"
-version = "0.3.1"
+version = "0.3.0"
 
 java {
     toolchain {
